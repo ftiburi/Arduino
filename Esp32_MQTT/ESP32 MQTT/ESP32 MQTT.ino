eadfe683af3ed48194fbcdfc3782225ef3a4b325
Fabio Tiburi <114815427+ftiburi@users.noreply.github.com>
2022-12-06 11:04:34 -0300

    MQTT ESP32 to ESP32

---
+++ Esp32_MQTT/ESP32 MQTT/ESP32 MQTT.ino
@@ -0,0 +1,133 @@
+
+
+
+#include <SPI.h>
+//#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+
+#define pinBotao1 18  //D5
+
+const char* host = "modernistic-discrep.000webhostapp.com";
+byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+WiFiMulti wifiMulti;
+
+int temperature = 2022;
+int sensorPin = A0;   // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int ledPincon = 12;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+//WiFi
+const char* SSID = "Sumig_Mobile";                // SSID / nome da rede WiFi que deseja se conectar
+const char* PASSWORD = "mobile@123";   // Senha da rede WiFi que deseja se conectar
+WiFiClient wifiClient; 
+//byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+//WiFiMulti wifiMulti;
+WiFiClient client;                       
+//MQTT Server
+const char* BROKER_MQTT = "mqtt.eclipseprojects.io"; //URL do broker MQTT que se deseja utilizar
+int BROKER_PORT = 1883;                      // Porta do Broker MQTT
+
+#define ID_MQTT  "BCI01"            //Informe um ID unico e seu. Caso sejam usados IDs repetidos a ultima conexão irá sobrepor a anterior. 
+#define TOPIC_PUBLISH "BCIBotao1"    //Informe um Tópico único. Caso sejam usados tópicos em duplicidade, o último irá eliminar o anterior.
+PubSubClient MQTT(wifiClient);        // Instancia o Cliente MQTT passando o objeto espClient
+
+//Declaração das Funções
+void mantemConexoes();  //Garante que as conexoes com WiFi e MQTT Broker se mantenham ativas
+void conectaWiFi();     //Faz conexão com WiFi
+void conectaMQTT();     //Faz conexão com Broker MQTT
+void enviaPacote();     //
+
+void setup() {
+  pinMode(pinBotao1, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);         
+
+  Serial.begin(115200);
+
+  conectaWiFi();
+  MQTT.setServer(BROKER_MQTT, BROKER_PORT);   
+}
+
+void loop() {
+  mantemConexoes();
+  enviaValores();
+  MQTT.loop();
+}
+
+void mantemConexoes() {
+    if (!MQTT.connected()) {
+       conectaMQTT(); 
+    }
+    
+    conectaWiFi(); //se não há conexão com o WiFI, a conexão é refeita
+}
+
+void conectaWiFi() {
+
+  if (WiFi.status() == WL_CONNECTED) {
+     return;
+  }
+        
+  Serial.print("Conectando-se na rede: ");
+  Serial.print(SSID);
+  Serial.println("  Aguarde!");
+
+  WiFi.begin(SSID, PASSWORD); // Conecta na rede WI-FI  
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(100);
+      Serial.print(".");
+  }
+  
+  Serial.println();
+  Serial.print("Conectado com sucesso, na rede: ");
+  Serial.print(SSID);  
+  Serial.print("  IP obtido: ");
+  Serial.println(WiFi.localIP()); 
+}
+
+void conectaMQTT() { 
+    while (!MQTT.connected()) {
+        Serial.print("Conectando ao Broker MQTT: ");
+        Serial.println(BROKER_MQTT);
+        if (MQTT.connect(ID_MQTT)) {
+            Serial.println("Conectado ao Broker com sucesso!");
+        } 
+        else {
+            Serial.println("Noo foi possivel se conectar ao broker.");
+            Serial.println("Nova tentatica de conexao em 10s");
+            delay(10000);
+        }
+    }
+}
+
+void enviaValores() {
+static bool estadoBotao1 = HIGH;
+static bool estadoBotao1Ant = HIGH;
+static unsigned long debounceBotao1;
+
+  estadoBotao1 = digitalRead(pinBotao1);
+  if (  (millis() - debounceBotao1) > 30 ) {  //Elimina efeito Bouncing
+     if (!estadoBotao1 && estadoBotao1Ant) {
+
+        //Botao Apertado     
+        MQTT.publish(TOPIC_PUBLISH, "1");
+        Serial.println("Botao1 APERTADO. Payload enviado.");
+        
+        debounceBotao1 = millis();
+     } else if (estadoBotao1 && !estadoBotao1Ant) {
+
+        //Botao Solto
+        MQTT.publish(TOPIC_PUBLISH, "0");
+        Serial.println("Botao1 SOLTO. Payload enviado.");
+        
+        debounceBotao1 = millis();
+     }
+     
+  }
+  estadoBotao1Ant = estadoBotao1;
+}
+
